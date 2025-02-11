dd64a0b2e663ff624568b4e9fea85c96661373b3
lucasmmo <lucasmocerinomonteiro@gmail.com>
2025-02-11 00:10:01 -0300

    refact: changed testes to better readness

--- web/backend/cmd/main_test.go
+++ web/backend/cmd/main_test.go
@@ -2,34 +2,77 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"net/http/httptest"
+	"parecer-gen/pkg/parecer"
 	"testing"
 )
 
-func TestCreateParecer(t *testing.T) {
-	req := httptest.NewRequest("POST", "/parecer", bytes.NewBufferString(`{"user":"user","creci":"creci","content":"content"}`))
-	req.Header.Set("Content-Type", "application/json")
+func TestHandleParecer(t *testing.T) {
+	t.Run("should create parecer without date", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/parecer", bytes.NewBufferString(`{"user":"user","creci":"creci","content":"content"}`))
+		req.Header.Set("Content-Type", "application/json")
 
-	w := httptest.NewRecorder()
+		w := httptest.NewRecorder()
 
-	CreateParecer(w, req)
+		CreateParecer(w, req)
 
-	if w.Code != 201 {
-		t.Errorf("expected status code 201, got %d", w.Code)
-		return
-	}
+		if w.Code != 201 {
+			t.Errorf("expected status code 201, got %d", w.Code)
+			return
+		}
+	})
+
+	t.Run("should create parecer with date", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/parecer", bytes.NewBufferString(`{"user":"user","creci":"creci","content":"content","date":"2025-02-07"}`))
+		req.Header.Set("Content-Type", "application/json")
+
+		w := httptest.NewRecorder()
+
+		CreateParecer(w, req)
+
+		if w.Code != 201 {
+			t.Errorf("expected status code 201, got %d", w.Code)
+			return
+		}
+	})
+
+	t.Run("should not create parecer with missing data", func(t *testing.T) {
+		req := httptest.NewRequest("POST", "/parecer", bytes.NewBufferString(`{"user":"user","creci":"creci","content":""}`))
+		req.Header.Set("Content-Type", "application/json")
+
+		w := httptest.NewRecorder()
+
+		CreateParecer(w, req)
+
+		if w.Code != 400 {
+			t.Errorf("expected status code 400, got %d", w.Code)
+			return
+		}
+	})
 }
 
-func TestGetParecer(t *testing.T) {
-	req := httptest.NewRequest("GET", "/parecer?id=parecer-1-01-0001", nil)
+func cleanUp(t *testing.T) {
+	req := httptest.NewRequest("GET", "/parecer", nil)
 	req.Header.Set("Accept", "application/json")
 
 	w := httptest.NewRecorder()
 
 	ReadParecer(w, req)
 
-	if w.Code != 200 {
-		t.Errorf("expected status code 200, got %d", w.Code)
-		return
+	var res []parecer.Data
+	if err := json.Unmarshal(w.Body.Bytes(), &res); err != nil {
+		t.Errorf("error unmarshaling response body: %s", err)
+	}
+
+	for _, parecer := range res {
+		req := httptest.NewRequest("DELETE", "/parecer?id="+parecer.ID, nil)
+		w := httptest.NewRecorder()
+		DeleteParecer(w, req)
+
+		if w.Code != 200 {
+			t.Errorf("expected status code 200, got %d", w.Code)
+			return
+		}
 	}
 }
